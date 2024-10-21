e40ca8d7c7f25c4c9e148cd9c0f2274d0cc05c04
pereia <wlsor15987@naver.com>
2024-10-21 14:51:28 +0900

    추가본

--- DHT-11/src/main.cpp
+++ DHT-11/src/main.cpp
@@ -1,19 +1,32 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+#define SOIL_MOISTURE_PIN A0
+#define RELAY_PIN 7
+
+int soilMoistureValue = 0;
+int thresholdMoisture = 600;
+float humidity = 0;
+float temperature = 0;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int a = 5;
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(SOIL_MOISTURE_PIN, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+
+  soilMoistureValue = analogRead(SOIL_MOISTURE_PIN);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
