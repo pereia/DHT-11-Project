65dd8b9b128052eba14cfb7d4ff9370285b7e16a
pereia <wlsor15987@naver.com>
2024-11-11 17:47:59 +0900

    코드는 끝

--- DHT-11/src/main.cpp
+++ DHT-11/src/main.cpp
@@ -17,6 +17,9 @@ int thresholdMoisture = 600;
 float humidity = 0;
 float temperature = 0;
 
+unsigned long previousMillis = 0;
+const long interval = 2000;
+
 
 void setup() {
   Serial.begin(9600);
@@ -28,12 +31,44 @@ void setup() {
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
+  if(BTSerial.available()){
+    String command = BTSerial.readString();
+    command.trim();
+
+    if (command =="ON"){
+      digitalWrite(RELAY_PIN,LOW);
+      Serial.println("수동으로 물 공급 시작.");
+      BTSerial.println("수동으로 물 공급 시작.");
+    }else if(command =="OFF"){
+      digitalWrite(RELAY_PIN,HIGH);
+      Serial.println("수동으로 물 공급 중지.");
+      BTSerial.println("수동으로 물 공급 중지.");
+    }
+    else if(command.startsWith("THRESHOLD")){
+      int newThreshold = command.substring(9).toInt();
+      thresholdMoisture = newThreshold;
+      Serial.print("새 토양 습도 기준값: ");
+      Serial.println(thresholdMoisture);
+      BTSerial.print("새 토양 습도 기준값: ");
+      BTSerial.println(thresholdMoisture);
+
+    }
+    
+    
+  }
+
+  if(currentMillis-previousMillis>=interval){
   humidity = dht.readHumidity();
   temperature = dht.readTemperature();
 
   soilMoistureValue = analogRead(SOIL_MOISTURE_PIN);
 
-  Serial.print("온도: ");
+  if(isnan(humidity)||isnan(temperature)){
+    Serial.println("DHT 센서 오류: 데이터를 읽을 수 없습니다.");
+    BTSerial.println("DHT 센서 오류");
+  }else{
+    Serial.print("온도: ");
   Serial.print(temperature);
   Serial.print("°C 습도: ");
   Serial.print(humidity);
@@ -47,6 +82,10 @@ void loop() {
   BTSerial.print("% 토양 습도: ");
   BTSerial.print(soilMoistureValue);
 
+  }
+
+  
+
   if(soilMoistureValue > thresholdMoisture){
     digitalWrite(RELAY_PIN, LOW);
     Serial.println("물을 공급합니다.");
@@ -56,8 +95,8 @@ void loop() {
     Serial.println("물 공급 중지.");
     BTSerial.println("물 공급 중지");
   }
-
-  delay(2000);
+  }
+  
 
 }
 
